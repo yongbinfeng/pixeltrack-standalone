960ac56dc2887798c19894df3987a179fc3d32cd
Philip Harris <violatingcp@gmail.com>
2021-09-20 05:21:58 +0000

    track outpus

--- src/cuda/plugin-Validation/CountValidatorSimple.cc
+++ src/cuda/plugin-Validation/CountValidatorSimple.cc
@@ -16,8 +16,8 @@
 #include <sstream>
 
 namespace {
-  std::atomic<int> allEvents = 0;
-  std::atomic<int> goodEvents = 0;
+  std::atomic<int> allEvents{0};
+  std::atomic<int> goodEvents{0};
 }  // namespace
 
 class CountValidatorSimple : public edm::EDProducer {
@@ -30,9 +30,10 @@ private:
   void endJob() override;
 
   //edm::EDGetTokenT<DigiClusterCount> digiClusterCountToken_;
-
   edm::EDGetTokenT<cms::cuda::Product<SiPixelDigisCUDA>> digiToken_;
   edm::EDGetTokenT<cms::cuda::Product<SiPixelClustersCUDA>> clusterToken_;
+  edm::EDGetTokenT<PixelTrackHeterogeneous> trackToken_;
+  edm::EDGetTokenT<ZVertexHeterogeneous> vertexToken_;
   uint32_t nModules_;
   uint32_t nClusters_;
   uint32_t  nDigis_;
@@ -46,9 +47,10 @@ private:
 
 CountValidatorSimple::CountValidatorSimple(edm::ProductRegistry& reg)
   : //digiClusterCountToken_(reg.consumes<DigiClusterCount>()),
-      digiToken_(reg.consumes<cms::cuda::Product<SiPixelDigisCUDA>>()),
-      clusterToken_(reg.consumes<cms::cuda::Product<SiPixelClustersCUDA>>()) {
-  std::cout << "---> Simple " <<  std::endl;
+  digiToken_(reg.consumes<cms::cuda::Product<SiPixelDigisCUDA>>()),
+  clusterToken_(reg.consumes<cms::cuda::Product<SiPixelClustersCUDA>>()),
+  trackToken_(reg.consumes<PixelTrackHeterogeneous>()),
+  vertexToken_(reg.consumes<ZVertexHeterogeneous>()) {
 }
 
 void CountValidatorSimple::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
@@ -58,7 +60,8 @@ void CountValidatorSimple::produce(edm::Event& iEvent, const edm::EventSetup& iS
   std::stringstream ss;
 
   ss << "Event " << iEvent.eventID() << " ";
-
+  unsigned int pCount = 0;
+  /*
   {
     auto const& pdigis = iEvent.get(digiToken_);
     cms::cuda::ScopedContextProduce ctx{pdigis};
@@ -83,22 +86,47 @@ void CountValidatorSimple::produce(edm::Event& iEvent, const edm::EventSetup& iS
     rawIdArr_  = digis.rawIdArrToHostAsync(ctx.stream());
     adc_       = digis.adcToHostAsync(ctx.stream());
     clus_      = digis.clusToHostAsync(ctx.stream());
+    output_ = new uint32_t[4*nDigis_+1];
     output_[0] = digis.nDigis();
     std::memcpy(output_ + 1,          rawIdArr_.get(),nDigis_*sizeof(uint32_t));
     std::memcpy(output_ + (nDigis_+1)  ,pdigi_.get()   ,nDigis_*sizeof(uint32_t));
     std::memcpy(output_ + (2*nDigis_+1),adc_.get()     ,nDigis_*sizeof(uint16_t));
-    std::memcpy(output_ + (3*nDigis_+1),clus_.get()    ,nDigis_*sizeof(int32_t));
+    std::memcpy(output_ + (3*nDigis_+1),clus_.get()    ,nDigis_*sizeof(uint32_t));
     //output_[1] = (uint32_t*)rawIdArr_.get();
     //output_[nDigis_+1]   = (uint32_t*)pdigi_.get();
     //output_[2*nDigis_+1] = (uint32_t*)adc_.get();
     //output_[3*nDigis_+1] = (uint32_t*) clus_.get();
-    std::cout << "--> formatting output" << std::endl;
-    for(unsigned int i0 = 0; i0 < nDigis_*4; i0++) {
-      std::cout << "---> " << i0 << " -- " << output_[i0] << std::endl;
-    }
-    std::cout << "--> formatting output done" << std::endl;
+    std::cout << " -- " << nDigis_ << "--> " << rawIdArr_.get()[0] << " -- " << pdigi_.get()[0]    << " -- " << adc_.get()[0] << " -- " << clus_.get()[0] << std::endl;
+    std::cout << " -- " << nDigis_ << "--> " << output_[1]         << " -- " << output_[nDigis_+1] << " -- " <<  output_[2*nDigis_+1] << " -- " <<  output_[3*nDigis_+1] << std::endl;
+    //pCount = 3*nDigis_+1;
+  } 
+  */
+  {
+    auto const& tracks = iEvent.get(trackToken_);
+    uint32_t nTracks = tracks->stride();
+    output_[0] = nTracks; pCount++;
+    std::memcpy(output_ + pCount,tracks->chi2.data()   ,nTracks*sizeof(float));   pCount+=nTracks;
+    std::memcpy(output_ + pCount,tracks->m_quality.data(),nTracks*sizeof(uint8_t)); pCount+=nTracks;
+    std::memcpy(output_ + pCount,tracks->eta.data()    ,nTracks*sizeof(float)); pCount+=nTracks;
+    std::memcpy(output_ + pCount,tracks->pt.data()     ,nTracks*sizeof(float)); pCount+=nTracks;
+    std::memcpy(output_ + pCount,tracks->hitIndices.begin(),5*nTracks*nTracks*sizeof(uint16_t)); pCount+=nTracks*nTracks*5;
+    std::memcpy(output_ + pCount,tracks->detIndices.begin(),5*nTracks*nTracks*sizeof(uint16_t)); pCount+=nTracks*nTracks*5;
+    //std::memcpy(output_ + pCount,tracks->stateAtBS,nTracks_*sizeof(uint8_t)); pCount+=nTracks_;
   }
-
+  {
+    static constexpr uint32_t MAXTRACKS = 32 * 1024;
+    static constexpr uint32_t MAXVTX = 1024;
+    auto const& vertices = iEvent.get(vertexToken_);
+    uint32_t nvtx = vertices->nvFinal;
+    output_[pCount] = nvtx;
+    std::memcpy(output_ + pCount,vertices->idv    ,MAXTRACKS*sizeof(int16_t));   pCount+=MAXTRACKS;
+    std::memcpy(output_ + pCount,vertices->zv     ,MAXVTX*sizeof(float));        pCount+=MAXVTX;
+    std::memcpy(output_ + pCount,vertices->wv     ,MAXVTX*sizeof(float));        pCount+=MAXVTX;
+    std::memcpy(output_ + pCount,vertices->chi2   ,MAXVTX*sizeof(float));        pCount+=MAXVTX;
+    std::memcpy(output_ + pCount,vertices->ptv2   ,MAXVTX*sizeof(float));        pCount+=MAXVTX;
+    std::memcpy(output_ + pCount,vertices->ndof   ,MAXVTX*sizeof(int32_t));      pCount+=MAXVTX;
+    std::memcpy(output_ + pCount,vertices->sortInd,MAXVTX*sizeof(uint16_t));     pCount+=MAXVTX;
+  }
   ++allEvents;
   if (ok) {
     ++goodEvents;
